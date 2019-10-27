34cbb905f1d04f8c60ae4c1032e212faa4cc9380
emilianomacias0 <emilianomacias0@gmail.com>
2019-10-27 12:46:29 -0600

    inyeccion de dependendias por medio de interfaz

--- src/main/java/com/emilianomacias0/app/di/controllers/IndexController.java
+++ src/main/java/com/emilianomacias0/app/di/controllers/IndexController.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.emilianomacias0.app.di.models.servicios.MiServicio;
+import com.emilianomacias0.app.di.models.servicios.IServicio;
 
 @Controller
 public class IndexController {
 
 	@Autowired
-	private MiServicio servicio;
+	private IServicio servicio;
 	@GetMapping({"","/","/index"})
 	public String index(Model model) {
 		model.addAttribute("objeto", servicio.proceso());
