4bdb1f8e95ad887f3b6709dfd801953b1bc737db
emilianomacias0 <emilianomacias0@gmail.com>
2019-10-27 14:38:59 -0600

    Ciclo de vida componentes

--- src/main/java/com/emilianomacias0/app/di/models/domain/Factura.java
+++ src/main/java/com/emilianomacias0/app/di/models/domain/Factura.java
@@ -2,6 +2,9 @@ package com.emilianomacias0.app.di.models.domain;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,6 +19,16 @@ public class Factura {
 	@Autowired
 	private List<ItemFactura> items;
 
+	@PostConstruct
+	public void inicializar() {
+		cliente.setNombre(cliente.getNombre().concat(" ").concat("Regina"));
+		descripcion = descripcion.concat(" del cliente ").concat(cliente.getNombre());
+	}
+	
+	@PreDestroy
+	public void destruir() {
+		System.out.println("Factura destruida ".concat(descripcion));
+	}
 	public String getDescripcion() {
 		return descripcion;
 	}
